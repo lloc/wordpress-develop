a2749f49983b1c23583b8bd2c9760a2caacf9689
Peter Wilson <peterwilsoncc@git.wordpress.org>
2025-05-05 01:41:14 +0000

    Build/Test Tools: Test version in additional config files.
    Introduces tests to ensure the version numbers in `package-lock.json` (two instances) and `composer.json` match the WordPress version specified in `version.php`.
    
    In pull requests, the `package-lock.json` file is updated automatically to match the version in `package.json`. The new test is intended to ensure the version numbers are correct in production branches.
    
    Fixes #63390.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@60219 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/basic.php
+++ tests/phpunit/tests/basic.php
@@ -65,4 +65,67 @@ class Tests_Basic extends WP_UnitTestCase {
 		$this->assertArrayHasKey( 'engines', $package_json );
 		$this->assertArrayHasKey( 'node', $package_json['engines'] );
 	}
+
+	/**
+	 * Test the version numbers in package-lock.json are correct.
+	 *
+	 * In pull requests, the package-lock.json file is updated automatically
+	 * to match the version in package.json. This test is intended to ensure
+	 * the version numbers are correct in production branches.
+	 *
+	 * @coversNothing
+	 *
+	 * @dataProvider data_package_lock_json
+	 */
+	public function test_package_lock_json( $path ) {
+		$package_lock_json = file_get_contents( dirname( ABSPATH ) . '/package-lock.json' );
+		$package_lock_json = json_decode( $package_lock_json, true );
+		list( $version )   = explode( '-', $GLOBALS['wp_version'] );
+
+		// package.json uses x.y.z, so fill cleaned $wp_version for .0 releases.
+		if ( 1 === substr_count( $version, '.' ) ) {
+			$version .= '.0';
+		}
+
+		$json_paths           = explode( '.', $path );
+		$package_lock_version = $package_lock_json;
+		foreach ( $json_paths as $json_path ) {
+			if ( ! isset( $package_lock_version[ $json_path ] ) ) {
+				$this->fail( "package-lock.json does not contain the path '$path'." );
+			}
+			$package_lock_version = $package_lock_version[ $json_path ];
+		}
+
+		$this->assertSame( $version, $package_lock_version, "package-lock.json's $path needs to be updated to $version." );
+	}
+
+	/**
+	 * Data provider for test_package_lock_json.
+	 *
+	 * @return array[] Data provider.
+	 */
+	public function data_package_lock_json() {
+		return array(
+			'top level' => array( 'version' ),
+			'package'   => array( 'packages..version' ),
+		);
+	}
+
+	/**
+	 * Test the version number in composer.json is correct.
+	 *
+	 * @coversNothing
+	 */
+	public function test_composer_json() {
+		$composer_json   = file_get_contents( dirname( ABSPATH ) . '/composer.json' );
+		$composer_json   = json_decode( $composer_json, true );
+		list( $version ) = explode( '-', $GLOBALS['wp_version'] );
+
+		// package.json uses x.y.z, so fill cleaned $wp_version for .0 releases.
+		if ( 1 === substr_count( $version, '.' ) ) {
+			$version .= '.0';
+		}
+
+		$this->assertSame( $version, $composer_json['version'], "composer.json's version needs to be updated to $version." );
+	}
 }
