a071d0c4719b221c777d934b796e6b654ca65bec
Dennis Ploetner <re@lloc.de>
2025-06-05 13:16:55 +0200

    Patch 48323 applied

--- src/wp-includes/ms-load.php
+++ src/wp-includes/ms-load.php
@@ -419,7 +419,8 @@ function ms_load_current_site_and_network( $domain, $path, $subdomain = false ) 
 
 		if ( $subdomain && ! defined( 'NOBLOGREDIRECT' ) ) {
 			// For a "subdomain" installation, redirect to the signup form specifically.
-			$destination .= 'wp-signup.php?new=' . str_replace( '.' . $current_site->domain, '', $domain );
+			$path        = 'wp-signup.php?new=' . str_replace( '.' . $current_site->domain, '', $domain );
+			$destination = apply_filters( 'network_site_url', $destination . $path, $path, $scheme );
 		} elseif ( $subdomain ) {
 			/*
 			 * For a "subdomain" installation, the NOBLOGREDIRECT constant
